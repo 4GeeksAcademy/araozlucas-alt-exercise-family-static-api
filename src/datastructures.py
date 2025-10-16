adb1209a903aca2dde22c5227986be45f890b770
Daniela González <danimge94@gmail.com>
2025-10-16 17:56:01 +0000

    lista estructura datos

--- src/datastructures.py
+++ src/datastructures.py
@@ -1,10 +1,3 @@
-"""
-Update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
-
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
@@ -26,19 +19,25 @@ class FamilyStructure:
         return generated_id
 
     def add_member(self, member):
-        ## You have to implement this method
-        ## Append the member to the list of _members
-        pass
+        if "id" not in member or member["id"] is None:
+            member["id"] = self._generate_id()
+        member["last_name"] = self.last_name()
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
-        ## You have to implement this method
-        ## Loop the list and delete the member with the given id
-        pass
+        for i, member in enumerate(self._members):
+            if member["id"] == id:
+                self._members.pop(i)
+                return True
+        return False
 
     def get_member(self, id):
-        ## You have to implement this method
-        ## Loop all the members and return the one with the given id
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
+        return None
+    
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
